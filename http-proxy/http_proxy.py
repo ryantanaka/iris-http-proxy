d5b665a6447dcde24c87312f7d4ceff2b2cf32a4
Ryan Tanaka <tanaka@isi.edu>
2020-04-11 00:01:32 -0700

    not hashing filenames; added two options for this

--- http-proxy/http_proxy.py
+++ http-proxy/http_proxy.py
@@ -1,11 +1,11 @@
 # A simple HTTP proxy which does caching of requests.
 # "Inspired" by: https://gist.github.com/justinmeiners/24dcf5904490b621220bed643651f681
-# but updated with 
-#- a clean exit on signal allowing it to be easily popped by another script using subprocess
-#- tcp socket reuse to avoid the tcp socket already in use if popped often
-#- a cache directory to avoid a lot of files just being in the middle
-# 
-# use it by getting 'http://localhost:8000/www.kernel.org' to get http://www.kernel.org 
+# but updated with
+# - a clean exit on signal allowing it to be easily popped by another script using subprocess
+# - tcp socket reuse to avoid the tcp socket already in use if popped often
+# - a cache directory to avoid a lot of files just being in the middle
+#
+# use it by getting 'http://localhost:8000/www.kernel.org' to get http://www.kernel.org
 # duplicate the class+tcpserver and listen on another port for your https needs (alter line 40 in the duplicate);
 # it will share the cache
 
@@ -17,40 +17,70 @@ import os
 import hashlib
 import signal
 
+from pathlib import Path
+from urllib.parse import urlparse
+
 cache_base = "./cache/"
 httpd = None
 
-def exit_gracefully(sig,stack):
+
+def exit_gracefully(sig, stack):
     print("received sig %d, quitting" % (sig))
     httpd.server_close()
     exit()
 
+
 class CacheHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-      m = hashlib.md5()
-      m.update(self.path.encode("utf-8"))
-      cache_filename = cache_base + m.hexdigest() +".cached"
+        # m = hashlib.md5()
+        # m.update(self.path.encode("utf-8"))
+        # cache_filename = cache_base + m.hexdigest() +".cached"
 
-      if not os.path.exists(cache_filename):
-          print("cache miss "+ self.path)
-          with open(cache_filename + ".temp", "wb") as output:
-            req = urllib.request.Request(self.path)
-            for k in self.headers:
-                if k not in ["Host"]:
-                    req.add_header(k, self.headers[k])
-            resp = urllib.request.urlopen(req)
-            shutil.copyfileobj(resp, output)
-            os.rename(cache_filename + ".temp", cache_filename)
-      else:
-          print("cache hit "+ self.path)
-      
-      with open(cache_filename, "rb") as cached:
-          self.send_response(200)
-          self.end_headers()
-          shutil.copyfileobj(cached, self.wfile)
+        url = urlparse(self.path)
+
+        # Option 1: file names aren't unique
+        cache_filename = (
+            cache_base
+            + url.netloc.replace(":", "-")
+            + url.path.replace("/", "-")
+            + ".cached"
+        )
+
+        """
+        # Option 2: file names are unique
+        cache_filename = cache_base + Path(url.path).name + ".cached"
+        """
+
+        print("cache_filename: {}".format(cache_filename))
+
+        if not os.path.exists(cache_filename):
+            print("cache miss " + self.path)
+
+            with open(cache_filename + ".temp", "wb") as output:
+                req = urllib.request.Request(self.path)
+
+                for k, v in self.headers.items():
+                    print(k, v)
+
+                    # for each header that is not "Host"
+                    # add that header to the request
+                    if k not in ["Host"]:
+                        req.add_header(k, self.headers[k])
+
+                resp = urllib.request.urlopen(req)
+                shutil.copyfileobj(resp, output)
+                os.rename(cache_filename + ".temp", cache_filename)
+        else:
+            print("cache hit " + self.path)
+
+        with open(cache_filename, "rb") as cached:
+            self.send_response(200)
+            self.end_headers()
+            shutil.copyfileobj(cached, self.wfile)
+
 
 signal.signal(signal.SIGINT, exit_gracefully)
-signal.signal(signal.SIGTERM,exit_gracefully)
+signal.signal(signal.SIGTERM, exit_gracefully)
 socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(("", 8000), CacheHandler)
 if not os.path.exists(cache_base):
